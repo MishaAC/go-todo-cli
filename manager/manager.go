5e5f48c418167946d37ab08ed14e280e5f1096c2
MishaAC <cristhianalbites233@gmail.com>
2026-01-22 11:13:12 -0500

    added constructor and methods for manager

--- manager/manager.go
+++ manager/manager.go
@@ -6,3 +6,30 @@ type Manager struct {
 	tasks  []task.Task
 	nextID int
 }
+
+func New() *Manager {
+	return &Manager{
+		tasks:  []task.Task{},
+		nextID: 1,
+	}
+}
+
+func (m *Manager) AddTask(title string) error {
+	return nil
+}
+
+func (m *Manager) ListTasks() []task.Task {
+	return nil
+}
+
+func (m *Manager) CompleteTask(id int) error {
+	return nil
+}
+
+func (m *Manager) DeleteTask(id int) error {
+	return nil
+}
+
+func (m *Manager) IsEmpty() bool {
+	return false
+}
